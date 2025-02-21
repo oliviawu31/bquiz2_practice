3cfa9c95c258791f478305b6a75e161f1c50acd8
oliviawu <oliviawu0301@gmail.com>
2025-02-21 16:23:17 +0800

    完成後台帳號管理頁面

--- web0220_B2/front/forgot.php
+++ web0220_B2/front/forgot.php
@@ -4,6 +4,7 @@
         <tr>
             <td>請輸入信箱以查詢密碼</td>
         </tr>
+        <tr>
         <td><input type="text" name="email" id="email" style="width:98%"></td>
         </tr>
         <tr>
@@ -11,9 +12,17 @@
         </tr>
         <tr>
             <td>
-                <input type="button" value="尋找">
+                <input type="button" value="尋找" onclick="forgot()">
             </td>
         </tr>
     </table>
-
 </fieldset>
+<script>
+function forgot() {
+    let email = $("#email").val()
+    $.get("./api/chk_email.php", {email}, (res) => {
+        $("#result").html(res)
+
+    })
+}
+</script>
