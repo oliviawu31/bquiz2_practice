b7b0a0d47300039e3cc7c4dada076ee335714bf4
oliviawu <oliviawu0301@gmail.com>
2025-02-24 16:14:25 +0800

    完成文章管理、問卷管理

--- web0220_B2/api/db.php
+++ web0220_B2/api/db.php
@@ -169,6 +169,8 @@ function to($url){
 
 $Total=new DB('total');
 $User=new DB('users');
+$News=new DB('news');
+$Que=new DB('que');
 
 
 if(!isset($_SESSION['view'])){
---
+++ web0220_B2/back/que.php
@@ -0,0 +1,47 @@
+<fieldset style="width:80%;margin:auto" >
+    <legend>
+        新增問卷
+    </legend>
+
+    <table style="width:100%">
+        <tr class='clo'>
+            <td>問卷名稱</td>
+            <td><input type="text" name="subject" id="subject" style="width:80%"></td>
+        </tr>
+        <tr class='clo'>
+            <td colspan='2'>
+                <div id="options">
+                    選項<input type="text" name="option[]" style="width:80%">
+                    <button onclick="more()">更多</button>
+                </div>
+            </td>
+            
+        </tr>
+    </table>
+    <div class="ct">
+        <button onclick="send()">新增</button>
+        <button onclick="resetForm()">清空</button>
+    </div>
+    
+</fieldset>
+
+<script>
+    function more(){
+        let el=`<div>選項<input type="text" name="option[]" style="width:80%"></div>`
+        $("#options").before(el)
+    }
+    function send(){
+        let subject=$("#subject").val()
+        let options=$("input[name='option[]']").map((id,item)=>$(item).val()).get()
+        // console.log(subject,options);
+        $.post("./api/add_que.php",{subject,options},(res)=>{
+            location.reload()
+        })
+        
+    }
+
+    function resetForm(){
+        $("input[type='text']").val("")
+    }
+
+</script>
