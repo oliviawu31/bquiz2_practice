f6921e00bb2041b46a1b74c307776b80ab1688e1
Olivia Wu <oliviawu0301@gmail.com>
2025-02-24 23:38:21 +0800

    完成分類網誌

--- web0220_B2/front/po.php
+++ web0220_B2/front/po.php
@@ -2,20 +2,40 @@
     目前位置:首頁>分類網誌><span id='type'>健康新知</span>
 </div>
 <style>
-    .type{
-        display:block;
-        margin:10px 0;
-    }
+.type,
+.list-item {
+    display: block;
+    margin: 10px 0;
+}
 </style>
 <fieldset style='width:150px;display:inline-block;vertical-align:top'>
     <legend>分類網誌</legend>
-    <a href="javascript:getList(1)" class='type'>健康新知</a>
-    <a href="javascript:getList(2)" class='type'>菸害防制</a>
-    <a href="javascript:getList(3)" class='type'>癌症防治</a>
-    <a href="javascript:getList(4)" class='type'>慢性病防治</a>
+    <a href="#" class='type' data-type='1'>健康新知</a>
+    <a href="#" class='type' data-type='2'>菸害防制</a>
+    <a href="#" class='type' data-type='3'>癌症防治</a>
+    <a href="#" class='type' data-type='4'>慢性病防治</a>
 </fieldset>
 
 
 <fieldset style='width:500px;display:inline-block'>
     <legend>文章列表</legend>
+    <div id="list"></div>
 </fieldset>
+
+<script>
+getList(1)
+
+$(".type").on('click', function() {
+    // console.log($(this).text())
+    $("#type").text($(this).text())
+    let type = $(this).data('type')
+    getList(type)
+})
+
+function getList(type) {
+    $("#list").load("./api/get_list.php", {
+        type
+    })
+
+}
+</script>
